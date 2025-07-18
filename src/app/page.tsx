f790b8383b184c634c0f77bb93d6bb25cf5ee3db
startupjafari <startupjafari@gmail.com>
2025-07-19 00:15:10 +0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <p>Главный блок</p>
+      </main>
+      <footer>
+        <p>Подвал</p>
+      </footer>
+    </div>
+  );
+}
